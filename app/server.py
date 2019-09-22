7cab846d81df041b49ab572e778f142e0d58d190
James Hart <jhart2083@gmail.com>
2019-09-22 22:27:02 +0200

    responding to client post request

--- app/server.py
+++ app/server.py
@@ -1,9 +1,22 @@
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse
-from starlette.middleware.cors import CORSMiddleware
-import sys
+import aiohttp
+import asyncio
 import uvicorn
+import sys
 
+# from fastai.vision import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+
+# from starlette.requests import Request
+from starlette.schemas import SchemaGenerator
+
+
+schemas = SchemaGenerator(
+    {"openapi": "3.0.0", "info": {"title": "Example API", "version": "1.0"}}
+)
 app = Starlette(debug=True)
 
 # A list of origins that should be permitted to make cross-origin requests
@@ -13,8 +26,23 @@ app.add_middleware(
         "http://localhost:8080",
         "https://bird-vue.onrender.com",
         "https://www.bird-vue.onrnender.com",
+        "http://localhost:8081",
     ],
 )
+
+
+@app.route("/upload", methods=["POST"])
+async def upload(request):
+    img_data = await request.form()
+    img_bytes = await (img_data["image"].read())
+    img = BytesIO(img_bytes)
+    # # prediction = learn.predict(img)[0]
+    return JSONResponse({"result": str(list(img))})
+    # return JSONResponse({"result": "Hello!"})
+
+
+# bytes = await (data["file"].read())
+# return predict_image_from_bytes(bytes)
 
 
 @app.route("/")
@@ -22,7 +50,12 @@ async def homepage(request):
     return JSONResponse({"hello": "world"})
 
 
+@app.route("/schema", methods=["GET"], include_in_schema=False)
+def openapi_schema(request):
+    return schemas.OpenAPIResponse(request=request)
+
+
 if __name__ == "__main__":
     if "serve" in sys.argv:
-        uvicorn.run(app, host="0.0.0.0", port=8080)
+        uvicorn.run(app, host="0.0.0.0", port=5000)
 
